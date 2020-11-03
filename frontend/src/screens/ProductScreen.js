2ef21804b560e39deb1af41344fd4be45c3663e6
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-11-03 22:34:02 +0530

    Implementing React Router

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            Productsdfsdfsdfsdfsdf
+        </div>
+    )
+}
+
+export default ProductScreen
