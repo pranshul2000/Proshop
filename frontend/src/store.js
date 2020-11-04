10a548d3e6697c7ba733661398c11bdbd0aa2924
Pranshul Gupta <pranshulgupta2000@gmail.com>
2020-11-04 23:02:53 +0530

    Product details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducer/productReducers";
+import { productListReducer, productDetailsReducer } from "./reducer/productReducers";
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
